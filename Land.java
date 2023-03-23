701335b7dd9443e80152409cc26aac2822d24e8f
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-23 21:58:32 +0100

    Update Land.java

--- Land.java
+++ Land.java
@@ -5,25 +5,27 @@ import java.util.*;
 
 import java.lang.Math;
 import javax.swing.*;
+
 /*
  * Les cases de la carte
  */
-public class Land extends JButton
-{
-	// Attributs **********************************************************************************
+public class Land extends JButton {
+	// Attributs *******************************************************************
 	// attribus servants la coloration des boutons
 	static final float maxHeight = 30;
-	static final float interSize = maxHeight/7;
+	static final float interSize = maxHeight / 7;
 	static int R, G, B;
 	// position du bouton dans la carte
 	private int Xcoord, Ycoord;
 	// contient l'entier de la case
 	public int cInt;
-	
-	// Constructeurs ******************************************************************************
-	// les paramètres du constructeur sont les coordonnées du bouton au sein de la carte
-	Land(int x, int y)
-	{
+
+	// Constructeurs ***************************************************************
+	/*
+	 * Les paramètres du constructeur sont les coordonnées du bouton au sein de la
+	 * carte
+	 */
+	Land(int x, int y) {
 		super();
 		// initialisation de la posistion du bouton dans la carte
 		Xcoord = x;
@@ -33,90 +35,76 @@ public class Land extends JButton
 		// le nombre que contient la case est affiché sur le bouton
 		this.setText("" + cInt);
 		// le nom du bouton est sa position dans la carte
-		this.setName(x+" "+y);
+		this.setName(x + ";" + y);
 		// on met à jour la couleur du bouton (elle dépend de la valeur qu'il contient)
 		UpdateLandColor();
 		// on ajoute un listener sur le bouton
-		this.addMouseListener(new java.awt.event.MouseAdapter()
-		{  
-			public void mousePressed(java.awt.event.MouseEvent evt)
-			{   
+		this.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mousePressed(java.awt.event.MouseEvent evt) {
 				// recupere le bouton enfonce
-				Land x = (Land) evt.getSource();
+				Land landSource = (Land) evt.getSource();
 				// recupere l'info
-				System.out.println("Nom :" + x.getName());
-				EditLandInterface editer = new EditLandInterface(x);
+				System.out.println("(" + landSource.getName() + ")");
+				EditInterface editer = new EditInterface(landSource);
 			}
 		});
 	}
-	
-	// Methodes ***********************************************************************************
-	// renvoie une couleur en fonction d'un entier
-	private Color getColor(int x)
-	{
-		if(x <= interSize)
-		{
+
+	// Methodes ********************************************************************
+	/*
+	 * Renvoie une couleur en fonction d'un entier
+	 */
+	private Color getColor(int x) {
+		if (x <= interSize) {
 			R = 0;
 			G = 0;
-			B = Math.round((7/maxHeight) * 255 * x);
-		}
-		else if(x <= 2*interSize)
-		{
+			B = Math.round((7 / maxHeight) * 255 * x);
+		} else if (x <= 2 * interSize) {
 			R = 0;
-			G = Math.round((7/maxHeight) * 255 * x - 255);
+			G = Math.round((7 / maxHeight) * 255 * x - 255);
 			B = 255;
-		}
-		else if(x <= 3*interSize)
-		{
+		} else if (x <= 3 * interSize) {
 			R = 0;
 			G = 255;
-			B = Math.round(- ((7/maxHeight) * 255 * x - (3 * 255)));
-		}
-		else if(x <= 4*interSize)
-		{
-			R = Math.round((7/maxHeight) * 255 * x - (3 * 255));
+			B = Math.round(-((7 / maxHeight) * 255 * x - (3 * 255)));
+		} else if (x <= 4 * interSize) {
+			R = Math.round((7 / maxHeight) * 255 * x - (3 * 255));
 			G = 255;
 			B = 0;
-		}
-		else if(x <= 5*interSize)
-		{
+		} else if (x <= 5 * interSize) {
 			R = 255;
-			G = Math.round(- ((7/maxHeight) * 255 * x - (5 * 255)));
+			G = Math.round(-((7 / maxHeight) * 255 * x - (5 * 255)));
 			B = 0;
-		}
-		else if(x <= 6*interSize)
-		{
+		} else if (x <= 6 * interSize) {
 			R = 255;
 			G = 0;
-			B = Math.round((7/maxHeight) * 255 * x - (5 * 255));
-		}
-		else
-		{
+			B = Math.round((7 / maxHeight) * 255 * x - (5 * 255));
+		} else {
 			R = 255;
-			G = Math.round((7/maxHeight) * 255 * x - (6 * 255));
+			G = Math.round((7 / maxHeight) * 255 * x - (6 * 255));
 			B = 255;
 		}
-		
-		//System.out.println("\nR = " + R + "\nG = " + G + "\nB = " + B);
 		Color lColor = new Color(R, G, B);
 		return lColor;
 	}
-	
-	// renvoie la coordonnée demandée
-	public int getXcoord()
-	{
+
+	/*
+	 * Renvoie la coordonnée demandée
+	 */
+	public int getXcoord() {
 		return Xcoord;
 	}
-	public int getYcoord()
-	{
+
+	public int getYcoord() {
 		return Ycoord;
 	}
-	
-	// met à jour la couleur du bouton
-	public void UpdateLandColor()
-	{
-		cInt = Integer.parseInt(this.getText());
-		this.setBackground(getColor(cInt));
+
+	/*
+	 * Met à jour la couleur du bouton à partir de l'entier
+	 */
+	public void UpdateLandColor() {
+		setText("" + cInt);
+		setBackground(getColor(cInt));
 	}
-	
+
 }
