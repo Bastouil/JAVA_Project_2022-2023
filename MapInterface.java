c07ace58b590003e562fced1eae7434b8bf541fb
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-23 21:56:38 +0100

    Update MapInterface.java

--- MapInterface.java
+++ MapInterface.java
@@ -1,64 +1,103 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.awt.Color.*;
 import java.util.*;
 
 import java.lang.Math;
 import javax.swing.*;
+
 /*
  * Interface de la carte.
  */
-public class MapInterface extends JFrame
-{
-	// Attributs **********************************************************************************
+public class MapInterface extends JFrame {
+	// Attributs *******************************************************************
+	private static final int SIZE_FRAME = 600;
+	protected int nbRows;
+	protected int nbCols;
 	protected BarMenu barMenu;
-	protected Land [][] Map;
+	protected Land[][] land;
+	protected Land startLand;
+	protected Land endLand;
 	protected JPanel frame;
-	static final int sizeFrame = 600;
-	
-	// Constructeurs ******************************************************************************
-	MapInterface(int nbRow, int nbCols)
-	{
+
+	// Constructeurs ***************************************************************
+	MapInterface(int nbRows, int nbCols) {
 		// paramètrage de l'interface
 		super("Interface principale");
-		this.setSize(sizeFrame,sizeFrame);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		// allocation du panneau
-		frame = new JPanel(new GridLayout(nbRow, nbCols));
-		
-		// allocation de la barre de menu
-		barMenu = new BarMenu();
-		this.setJMenuBar(barMenu);
-		// allocation du tableau de bouton/entier
-		Map = new Land [nbRow][nbCols];
-		for(int j= 0; j < nbCols; j++)
-		{
-			for(int i= 0; i < nbRow; i++)
-			{
-				// allocation d'un bouton/entier
-				Map[i][j] = new Land(i, j);
+		setSize(SIZE_FRAME, SIZE_FRAME);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// on garde les dimensions de la carte
+		this.nbRows = nbRows;
+		this.nbCols = nbCols;
+
+		// instanciation du panneau
+		frame = new JPanel(new GridLayout(nbRows, nbCols));
+
+		// instanciation du tableau de cases
+		land = new Land[nbRows][nbCols];
+		for (int i = 0; i < nbRows; i++) {
+			for (int j = 0; j < nbCols; j++) {
+				// instanciation d'un case
+				land[i][j] = new Land(i, j);
 				// ajout de du bouton au panneau
-				frame.add(Map[i][j]);
+				frame.add(land[i][j]);
 			}
 		}
-		
+
+		// instanciation de la barre de menu
+		barMenu = new BarMenu();
+		setJMenuBar(barMenu);
+		// ajout des listener sur les sous-menus
+		barMenu.saveFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Choix \"Enregistrer\"");
+				EditInterface editer = new EditInterface(GetMapInterface());
+				editer.AddListenerFileWrite(GetMapInterface());
+			}
+		});
+		barMenu.loadFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Choix \"Charger un fichier\"");
+				EditInterface editer = new EditInterface(GetMapInterface());
+				editer.AddListenerFileRead(GetMapInterface());
+			}
+		});
+		barMenu.selectSnE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Choix \"Selectionner le depart et l'arriver\"");
+				// TODO
+			}
+		});
+		barMenu.launchResolution.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Choix \"Lancer la resolution\"");
+				// TODO
+			}
+		});
+
 		// ajout du panneau et de la barre de menu à l'interface
-		this.getContentPane().add(frame, BorderLayout.CENTER);
-		this.getContentPane().add(barMenu, BorderLayout.NORTH);
-		this.setVisible(true);
+		getContentPane().add(frame, BorderLayout.CENTER);
+		getContentPane().add(barMenu, BorderLayout.NORTH);
+		setVisible(true);
 	}
 
-	// Methodes ***********************************************************************************
-	// met à jour la couleur de la carte
-	protected void UpdateMapColor()
-	{
-		for(int j= 0; j < Map[0].length; j++)
-		{
-			for(int i= 0; i < Map.length; i++)
-			{
-				Map[i][j].UpdateLandColor();
+	// Methodes ********************************************************************
+	/*
+	 * Met à jour la couleur de la carte
+	 */
+	protected void UpdateMapColor() {
+		for (int j = 0; j < land[0].length; j++) {
+			for (int i = 0; i < land.length; i++) {
+				land[i][j].UpdateLandColor();
 			}
 		}
+	}
+
+	/*
+	 * Récuperer le pointeur du la structure MapInterface
+	 */
+	private MapInterface GetMapInterface() {
+		return this;
 	}
 }
