c61e6a4de29e891decafb27e0ecd44ab68ef858d
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-24 22:02:30 +0100

    Implémentation des listeners de sélection D/A
    Implémentation des listeners de sélection de départ et d'arrivée.

--- MapInterface.java
+++ MapInterface.java
@@ -12,6 +12,11 @@ import javax.swing.*;
  */
 public class MapInterface extends JFrame {
 	// Attributs *******************************************************************
+	// codes utilisées pour saisir les différents listeners
+	public static final int EDIT_ACTION = 1;
+	public static final int START_ACTION = 2;
+	public static final int END_ACTION = 3;
+	// taille des côtées de la fenêtre
 	private static final int SIZE_FRAME = 600;
 	protected int nbRows;
 	protected int nbCols;
@@ -20,6 +25,7 @@ public class MapInterface extends JFrame {
 	protected Land startLand;
 	protected Land endLand;
 	protected JPanel frame;
+	protected JLabel message;
 
 	// Constructeurs ***************************************************************
 	MapInterface(int nbRows, int nbCols) {
@@ -27,6 +33,9 @@ public class MapInterface extends JFrame {
 		super("Interface principale");
 		setSize(SIZE_FRAME, SIZE_FRAME);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// instanciation du message
+		message = new JLabel("Cliquez sur une case pour la modifier");
+		message.setHorizontalAlignment(message.CENTER);
 		// on garde les dimensions de la carte
 		this.nbRows = nbRows;
 		this.nbCols = nbCols;
@@ -39,12 +48,15 @@ public class MapInterface extends JFrame {
 		for (int i = 0; i < nbRows; i++) {
 			for (int j = 0; j < nbCols; j++) {
 				// instanciation d'un case
-				land[i][j] = new Land(i, j);
+				land[i][j] = new Land(this, i, j);
 				// ajout de du bouton au panneau
 				frame.add(land[i][j]);
+				land[i][j].addMouseListener(Land.editLandListener);
 			}
 		}
-
+		// départ et arrivée de base sur la case (0;0)
+		startLand = land[0][0];
+		endLand = land[0][0];
 		// instanciation de la barre de menu
 		barMenu = new BarMenu();
 		setJMenuBar(barMenu);
@@ -52,6 +64,7 @@ public class MapInterface extends JFrame {
 		barMenu.saveFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Choix \"Enregistrer\"");
+				message.setText("Enregistrement d'un fichier");
 				EditInterface editer = new EditInterface(GetMapInterface());
 				editer.AddListenerFileWrite(GetMapInterface());
 			}
@@ -59,37 +72,56 @@ public class MapInterface extends JFrame {
 		barMenu.loadFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Choix \"Charger un fichier\"");
+				message.setText("Chargement d'un fichier");
 				EditInterface editer = new EditInterface(GetMapInterface());
 				editer.AddListenerFileRead(GetMapInterface());
 			}
 		});
-		barMenu.selectSnE.addActionListener(new ActionListener() {
+		barMenu.selectStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("Choix \"Selectionner le depart et l'arriver\"");
-				// TODO
+				System.out.println("Choix \"Selectionner le depart\"");
+				message.setText("Veuillez selectionner la case de départ");
+				// change les listeners ce trouvant sur les boutons
+				DefineLandsListener(START_ACTION);
+			}
+		});
+		barMenu.selectEnd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Choix \"Selectionner l'arrivee\"");
+				message.setText("Veuillez selectionner la case d'arrivée");
+				// change les listeners ce trouvant sur les boutons
+				DefineLandsListener(END_ACTION);
 			}
 		});
 		barMenu.launchResolution.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Choix \"Lancer la resolution\"");
 				// TODO
+				
 			}
 		});
 
 		// ajout du panneau et de la barre de menu à l'interface
 		getContentPane().add(frame, BorderLayout.CENTER);
 		getContentPane().add(barMenu, BorderLayout.NORTH);
+		getContentPane().add(message, BorderLayout.SOUTH);
 		setVisible(true);
 	}
 
 	// Methodes ********************************************************************
+	/*
+	 * Met le message par défaut sur le JLabel
+	 */
+	public void DefaultMessage() {
+		message.setText("Cliquez sur une case pour la modifier");
+	}
 	/*
 	 * Met à jour la couleur de la carte
 	 */
 	protected void UpdateMapColor() {
 		for (int j = 0; j < land[0].length; j++) {
 			for (int i = 0; i < land.length; i++) {
-				land[i][j].UpdateLandColor();
+				land[i][j].UpdateAppearance();
 			}
 		}
 	}
@@ -99,5 +131,41 @@ public class MapInterface extends JFrame {
 	 */
 	private MapInterface GetMapInterface() {
 		return this;
+	}
+	/*
+	 * Définit les listeners à mettre sur les boutons (objets de type Land)
+	 */
+	// TODO
+	public void DefineLandsListener(int CdeListener) {
+		if(CdeListener == EDIT_ACTION) {
+			for (int i = 0; i < nbRows; i++) {
+				for (int j = 0; j < nbCols; j++) {
+					for (MouseListener oldListener : land[i][j].getMouseListeners()) {
+						land[i][j].removeMouseListener(oldListener);
+					}
+					land[i][j].addMouseListener(Land.editLandListener);
+				}
+			}
+		}
+		else if(CdeListener == START_ACTION) {
+			for (int i = 0; i < nbRows; i++) {
+				for (int j = 0; j < nbCols; j++) {
+					for (MouseListener oldListener : land[i][j].getMouseListeners()) {
+						land[i][j].removeMouseListener(oldListener);
+					}
+					land[i][j].addMouseListener(Land.startLandListener);
+				}
+			}
+		}
+		else if(CdeListener == END_ACTION) {
+			for (int i = 0; i < nbRows; i++) {
+				for (int j = 0; j < nbCols; j++) {
+					for (MouseListener oldListener : land[i][j].getMouseListeners()) {
+						land[i][j].removeMouseListener(oldListener);
+					}
+					land[i][j].addMouseListener(Land.endLandListener);
+				}
+			}
+		}
 	}
 }
