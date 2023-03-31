599958f6267836752fd315ea529610079d90f8af
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-31 08:56:48 +0200

    Update App.java

--- App.java
+++ App.java
@@ -7,6 +7,6 @@ public class App
 	public static void main(String[] args)
 	{
 		// interface de la carte (les arguments sont la taille de la carte) 
-		MapInterface map = new MapInterface(10, 10);
+		MapInterface map = new MapInterface(8, 8);
 	}
 }
