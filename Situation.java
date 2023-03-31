6da336defca95233974c95d8dce87106f99a3498
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-31 09:02:54 +0200

    Update Situation.java

--- Situation.java
+++ Situation.java
@@ -1,5 +1,13 @@
+/*
+ * INF 1401 : projet JAVA 2022-2023
+ * Trouver le meilleur chemin au sein d'une grille de case avec des nombres.
+ * JAOUANNE Lilian & GARCON Bastian
+ */
 
-public class Situation {
+/*
+ * Situation qui sera stockée dans la pile
+ */
+public class Situation{
 	int lastChoice;
 	Land lastLand;
 	
