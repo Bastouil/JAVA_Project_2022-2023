cab3f150ffeeee969cb4fa1bd6222a46fdb5f0bd
Bastouil <116286115+Bastouil@users.noreply.github.com>
2023-03-31 09:05:22 +0200

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,62 +1,131 @@
+/*
+ * INF 1401 : projet JAVA 2022-2023
+ * Trouver le meilleur chemin au sein d'une grille de case avec des nombres.
+ * JAOUANNE Lilian & GARCON Bastian
+ */
+
 import java.util.Vector;
 
-public class Stack<T> {
-	int nbMove;
+/*
+ * Classe contenant la pile du meilleur chemin et celle du chemin courant. Elle
+ * contient aussi le coût des deux piles.
+ */
+public class Stack<T extends Situation> {
 	int costPath;
 	Vector<T> table;
 
+	int bestCostPath;
+	Vector<T> bestTable;
+
 	Stack() {
-		nbMove = 0;
 		costPath = 0;
 		table = new Vector<T>();
+
+		bestCostPath = Integer.MAX_VALUE;
+		bestTable = new Vector<T>();
 	}
 
 	void Push(T x) {
 		table.add(table.size(), x);
 	}
+	void PushBestTable(T x) {
+		bestTable.add(bestTable.size(), x);
+	}
 
 	T TopStack() {
 		return table.elementAt(table.size() - 1);
+	}
+	T TopStackBestTable() {
+		return bestTable.elementAt(bestTable.size() - 1);
 	}
 
 	void Pop() {
 		table.remove(table.size() - 1);
 	}
-	/*
-	 * Renvoie une copie de la pile jusqu'à avant la situation donnée en partant du
-	 * bas de la pile
-	 */
-	Stack<Situation> DuplicateStackSituationUntil(Situation situationToStop) {
-		Stack<Situation> newStack = new Stack<Situation>();
-		Situation actualSituation;
-		newStack.nbMove = 0;
-		newStack.costPath = 0;
-		int i = 0;
-		actualSituation = (Situation) table.elementAt(i);
-		while (actualSituation != situationToStop){
-			newStack.Push(actualSituation.DuplicateSituation());
-			nbMove++;
-			if (i >= 1)
-				costPath += ((Situation)table.elementAt(i-1)).lastLand.CostTo(actualSituation.lastLand);
-			i++;
-			if (i < table.size())
-				actualSituation = (Situation) table.elementAt(i);
+	void PopBestTable() {
+		bestTable.remove(bestTable.size() - 1);
+	}
+	void ClearTable() {
+		int i = table.size();
+		while (i > 0) {
+			Pop();
+			i--;
 		}
-		
-		return newStack;
+	}
+	void ClearBestTable() {
+		int i = bestTable.size();
+		while (i > 0) {
+			PopBestTable();
+			i--;
+		}
+	}
+	void Clear() {
+		ClearTable();
+		ClearBestTable();
+		costPath = 0;
+		bestCostPath = Integer.MAX_VALUE;
 	}
 	/*
-	 * Renvoie la situation avant celle donnée en paramètre
+	 * Renvoie une copie de la pile jusqu'à avant la situation donnée en partant du
+	 * bas de la pile. Attention, le coût n'est pas mis à jour.
 	 */
-	Situation SituationBefore(Situation situationToStop) {
-		int i = 0;
-		Situation previousSituation = null;
-		Situation situation = (Situation) table.elementAt(i);
-		while(situation != situationToStop) {
-			previousSituation = (Situation) table.elementAt(i);
-			i++;
-			situation = (Situation) table.elementAt(i);
+	void DuplicateBestTableIntoTableUntil(T elementToStop) {
+		ClearTable();
+		if (bestTable.size() <= 0) {
+			System.out.println("La meilleur pile est vide -> \"DuplicateBestTableIntoTableUntil\" stop");
+			return;
 		}
-		return previousSituation;
+		T actualElement;
+		int i = 0;
+		actualElement = bestTable.elementAt(i);
+		while (actualElement != elementToStop) {
+			Push(DuplicateElement(actualElement));
+			i++;
+			actualElement = bestTable.elementAt(i);
+		}
+	}
+	/*
+	 * Renvoie une copie de la pile en partant du bas de la pile. Attention, le coût
+	 * n'est pas mis à jour.
+	 */
+	void DuplicateTableIntoBestTable() {
+		ClearBestTable();
+		T element;
+		int i = 0;
+		int sizeTable = table.size();
+		while (i < sizeTable) {
+			element = table.elementAt(i);
+			PushBestTable(DuplicateElement(element));
+			i++;
+		}
+	}
+
+	/*
+	 * Duplique un élément T
+	 */
+	T DuplicateElement(T x) {
+		int choice = x.lastChoice;
+		Land land = x.lastLand;
+		
+		return (T) new Situation(choice, land);
+	}
+
+	/*
+	 * Renvoie l'élément avant celle donnée en paramètre
+	 */
+	T ElementBefore(Vector<T> t, T elementToStop) {
+		if (t.size() <= 0) {
+			System.out.println("La pile donnee est vide -> \"ElementBefore\" stop");
+			return null;
+		}
+		int i = 0;
+		T previousElement = null;
+		T situation = t.elementAt(i);
+		while (situation != elementToStop) {
+			previousElement = t.elementAt(i);
+			i++;
+			situation = t.elementAt(i);
+		}
+		return previousElement;
 	}
 }
